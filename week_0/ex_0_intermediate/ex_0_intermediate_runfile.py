874bfd99425e1c305e51343c047b12c542415f9a
salomescarfone <salome.scarfone-ynglada@utoulouse.fr>
2025-11-02 12:01:22 +0100

    Submission 3 Homework 1 Intermediate

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,10 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
+import math
+
+initial_count = float(input("Enter the initial count:"))
+final_count = float(input("Enter the final count:"))
+time = float(input("Enter the time of growth :"))
+
+growth_rate = (math.log(final_count) - math.log(initial_count))/time
+
+print("The growth rate of the microbial culture is:", growth_rate)
