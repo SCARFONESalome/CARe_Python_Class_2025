a7fd8c3fc7f5e4467ddec37049e36ed99011686e
salomescarfone <salome.scarfone-ynglada@utoulouse.fr>
2025-11-09 13:35:53 +0100

    Submission 6 Homework 1 Beginner

--- week_1/ex_1_beginner/ex_1_beginner_runfile.py
+++ week_1/ex_1_beginner/ex_1_beginner_runfile.py
@@ -1,9 +1,25 @@
 #YOUR CODE FOR EX_1 BEGINNER HERE
-x = 10
 
-def example_function():
-    x = 5
-    print("Local variable x:", x)
+data = input("Enter population count of microbial culture for 5 following days:").split()
 
-example_function()
-print("Outside function, global x is still accessible:", x)
+if len(data) == 5 and all(item.isdigit() for item in data):
+        print("Values are 5 numbers so they are valid")
+        data = [int(item) for item in data]
+
+        if all(data[i] < data[i + 1] for i in range(4)):
+            for i in range(1, 6):
+                print(f"day {i} -> {data[i-1]}")
+
+            print(f"Maximum: {max(data)}")
+            print(f"Minimum: {min(data)}")
+            average = sum(data) / len(data)
+            print(f"Average: {average}")
+
+            for i in range(5):
+                if data[i] > 200:
+                    print(f"Population exceeded 200 on day {i + 1} with a value of: {data[i]}")
+                    break
+        else:
+            print("But, values need to increase days after days")
+else:
+    print("Values are not valid, please enter 5 increasing numbers")
